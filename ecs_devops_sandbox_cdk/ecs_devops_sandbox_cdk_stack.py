a3e9dc65642117ccc789656d078d380c8299c63e
Jenna Pederson <jennapederson@gmail.com>
2022-05-16 09:34:50 -0500

    Convert to cdk2

--- ecs_devops_sandbox_cdk/ecs_devops_sandbox_cdk_stack.py
+++ ecs_devops_sandbox_cdk/ecs_devops_sandbox_cdk_stack.py
@@ -1,19 +1,58 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
-from constructs import Construct
+import aws_cdk as cdk
+import aws_cdk.aws_ecr as ecr
+import aws_cdk.aws_ec2 as ec2
+import aws_cdk.aws_ecs as ecs
+import aws_cdk.aws_iam as iam
 
-class EcsDevopsSandboxCdkStack(Stack):
+class EcsDevopsSandboxCdkStack(cdk.Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        ecr_repository = ecr.Repository(self,
+            "ecs-devops-sandbox-repository",
+            repository_name="ecs-devops-sandbox-repository")
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "EcsDevopsSandboxCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        vpc = ec2.Vpc(self,
+            "ecs-devops-sandbox-vpc",
+            max_azs=3)
+
+        cluster = ecs.Cluster(self,
+            "ecs-devops-sandbox-cluster",
+            cluster_name="ecs-devops-sandbox-cluster",
+            vpc=vpc)
+
+        execution_role = iam.Role(self,
+            "ecs-devops-sandbox-execution-role",
+            assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+            role_name="ecs-devops-sandbox-execution-role")
+
+        execution_role.add_to_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            resources=["*"],
+            actions=[
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+                ]
+        ))
+
+        task_definition = ecs.FargateTaskDefinition(self,
+            "ecs-devops-sandbox-task-definition",
+            execution_role=execution_role,
+            family="ecs-devops-sandbox-task-definition")
+
+        container = task_definition.add_container(
+            "ecs-devops-sandbox",
+            image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample"),
+            logging=ecs.LogDrivers.aws_logs(stream_prefix="ecs-devops-sandbox-container")
+        )
+
+        service = ecs.FargateService(self,
+            "ecs-devops-sandbox-service",
+            cluster=cluster,
+            task_definition=task_definition,
+            service_name="ecs-devops-sandbox-service")
