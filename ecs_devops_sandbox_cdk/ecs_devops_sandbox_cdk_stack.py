0aecb38bdf3984743bed8b7845fe88c36533a2e6
Jenna Pederson <jennapederson@gmail.com>
2022-05-16 09:35:24 -0500

    Use loadbalancer

--- ecs_devops_sandbox_cdk/ecs_devops_sandbox_cdk_stack.py
+++ ecs_devops_sandbox_cdk/ecs_devops_sandbox_cdk_stack.py
@@ -3,6 +3,7 @@ import aws_cdk.aws_ecr as ecr
 import aws_cdk.aws_ec2 as ec2
 import aws_cdk.aws_ecs as ecs
 import aws_cdk.aws_iam as iam
+import aws_cdk.aws_ecs_patterns as ecs_patterns
 
 class EcsDevopsSandboxCdkStack(cdk.Stack):
 
@@ -40,19 +41,38 @@ class EcsDevopsSandboxCdkStack(cdk.Stack):
                 ]
         ))
 
-        task_definition = ecs.FargateTaskDefinition(self,
-            "ecs-devops-sandbox-task-definition",
-            execution_role=execution_role,
-            family="ecs-devops-sandbox-task-definition")
+        # task_definition = ecs.FargateTaskDefinition(self,
+        #     "ecs-devops-sandbox-task-definition",
+        #     execution_role=execution_role,
+        #     family="ecs-devops-sandbox-task-definition")
 
-        container = task_definition.add_container(
-            "ecs-devops-sandbox",
+        # container = task_definition.add_container(
+        #     "ecs-devops-sandbox",
+        #     image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample"),
+        #     logging=ecs.LogDrivers.aws_logs(stream_prefix="ecs-devops-sandbox-container")
+        # )
+
+        # service = ecs.FargateService(self,
+        #     "ecs-devops-sandbox-service",
+        #     cluster=cluster,
+        #     task_definition=task_definition,
+        #     service_name="ecs-devops-sandbox-service")
+
+        task_image_options = ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+            family="ecs-devops-sandbox-task-definition",
+            execution_role=execution_role,
             image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample"),
-            logging=ecs.LogDrivers.aws_logs(stream_prefix="ecs-devops-sandbox-container")
+            container_name="ecs-devops-sandbox",
+            container_port=8080,
+            log_driver=ecs.LogDrivers.aws_logs(stream_prefix="ecs-devops-sandbox-container")
         )
 
-        service = ecs.FargateService(self,
-            "ecs-devops-sandbox-service",
+        ecs_patterns.ApplicationLoadBalancedFargateService(self, "ecs-devops-sandbox-service",
             cluster=cluster,
-            task_definition=task_definition,
-            service_name="ecs-devops-sandbox-service")
+            service_name="ecs-devops-sandbox-service",
+            # cpu=512,                    # Default is 256
+            desired_count=2,            # Default is 1
+            task_image_options=task_image_options,
+            memory_limit_mib=512,      # Default is 512
+            public_load_balancer=True # Default is False
+        )
